ebd40b9daf5ebd93499c35f8c18368649125352f
tacadh <53230229+tacadh@users.noreply.github.com>
2019-10-11 09:59:36 -0500

    Update formEntries.js

--- medications/formEntries.js
+++ medications/formEntries.js
@@ -1,38 +1,38 @@
 
-            {
-              title: 'aminosalicylates',
-              options: ['no', 'sulfasalazine', 'otherAminosalicylates']
-            },
-            {
-              title: 'methotrexate',
-              options: ['no', 'yes']
-            },
-            {
-              title: 'allopurinol',
-              options: ['no', 'yes']
-            },
-            {
-              title: 'thiopurinesIncluding',
-              options: ['no', 'yes']
-            },
-            {
-              title: 'biologics',
-              options: ['no', 'antiTnfs', 'vedolizumab', 'ustekinumab']
-            },
-            {
-              title: 'smallMolecules',
-              options: ['no', 'yes']
-            },
-            {
-              title: 'antibiotics',
-              options: ['no', 'ciprofloxacin', 'metronidazole', 'vancomycin']
-            },
-            {
-              title: 'steriods',
-              options: ['no', 'prednisone', 'budesonide', 'intravenous']
-            },
-            {
-              title: 'test',
-              options: ['no', 'test2']
-            }
-          
+{
+  title: 'aminosalicylates',
+  options: ['no', 'sulfasalazine', 'otherAminosalicylates']
+},
+{
+  title: 'methotrexate',
+  options: ['no', 'yes']
+},
+{
+  title: 'allopurinol',
+  options: ['no', 'yes']
+},
+{
+  title: 'thiopurinesIncluding',
+  options: ['no', 'yes']
+},
+{
+  title: 'biologics',
+  options: ['no', 'antiTnfs', 'vedolizumab', 'ustekinumab']
+},
+{
+  title: 'smallMolecules',
+  options: ['no', 'yes']
+},
+{
+  title: 'antibiotics',
+  options: ['no', 'ciprofloxacin', 'metronidazole', 'vancomycin']
+},
+{
+  title: 'steriods',
+  options: ['no', 'prednisone', 'budesonide', 'intravenous']
+},
+{
+  title: 'test',
+  options: ['no', 'test2']
+}
+
