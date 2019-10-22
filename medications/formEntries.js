a23856b8292fb3b9265191f7cb5def37eff13cb5
tacadh <53230229+tacadh@users.noreply.github.com>
2019-10-22 12:12:40 -0500

    Update formEntries.js

--- medications/formEntries.js
+++ medications/formEntries.js
@@ -1,36 +1,36 @@
-{
-  title: 'aminosalicylates',
-  options: ['no', 'sulfasalazine', 'otherAminosalicylates']
+[{
+  "title": "aminosalicylates",
+  "options": ["no", "sulfasalazine", "otherAminosalicylates"]
 },
 {
-  title: 'methotrexate',
-  options: ['no', 'yes']
+  "title": "methotrexate",
+  "options": ["no", "yes"]
 },
 {
-  title: 'allopurinol',
-  options: ['no', 'yes']
+  "title": "allopurinol",
+  "options": ["no", "yes"]
 },
 {
-  title: 'thiopurinesIncluding',
-  options: ['no', 'yes']
+  "title": "thiopurinesIncluding",
+  "options": ["no", "yes"]
 },
 {
-  title: 'biologics',
-  options: ['no', 'antiTnfs', 'vedolizumab', 'ustekinumab']
+  "title": "biologics",
+  "options": ["no", "antiTnfs", "vedolizumab", "ustekinumab"]
 },
 {
-  title: 'smallMolecules',
-  options: ['no', 'yes']
+  "title": "smallMolecules",
+  "options": ["no", "yes"]
 },
 {
-  title: 'antibiotics',
-  options: ['no', 'ciprofloxacin', 'metronidazole', 'vancomycin']
+  "title": "antibiotics",
+  "options": ["no", "ciprofloxacin", "metronidazole", "vancomycin"]
 },
 {
-  title: 'steriods',
-  options: ['no', 'prednisone', 'budesonide', 'intravenous']
+  "title": "steriods",
+  "options": ["no", "prednisone", "budesonide", "intravenous"]
 },
 {
-  title: 'test',
-  options: ['no', 'test2']
-}
+  "title": "test",
+  "options": ["no", "test2"]
+}]
