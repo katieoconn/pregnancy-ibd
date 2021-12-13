23fc9965ccb801ed8f56feb2da8283b038f9a29b
Jared Mosher <jared.mosher@gmail.com>
2021-12-13 11:59:25 -0700

    Create formEntries2.js

---
+++ medications/formEntries2.js
@@ -0,0 +1,39 @@
+[{ 
+ "title": "corticosteroids",
+  "options": ["no", "budesonide", "prednisone", "prednisolone", "methylprednisolone", "hydrocortisone"]
+},
+{
+  "title": "5-aminosalicylates",
+  "options": ["no", "mesalamine", "sulfasalazine"	]
+},
+{ 
+ "title": "immunomodulator",
+  "options": ["no", "imuran", "purinethol", "methotrexate", "allopurinol"]
+}
+{
+  "title": "thiopurinesIncluding",
+  "options": ["no", "yes"]
+},
+{
+  "title": "biologics",
+  "options": ["no", "antiTnfs", "ustekinumab", "vedolizumab"]
+},
+{
+  "title": "smallMolecules",
+  "options": ["no", "tofacitinib"]
+},
+  {
+  "title": "calcineurinInhibitor",
+  "options": ["no", "cyclosporin"]
+},
+{
+  "title": "antibiotics",
+  "options": ["no", "ciprofloxacin", "metronidazole", "vancomycin"]
+},
+{
+  "title": "steroids",
+  "options": ["no", "prednisone", "budesonide", "intravenous"]
+},
+
+
+  
