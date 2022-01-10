e2d6cc25f4acd95f22885211db82d124390f239c
tacadh <53230229+tacadh@users.noreply.github.com>
2022-01-10 14:11:02 -0600

    Create medicationColourCoded.js

---
+++ medications/medicationColourCoded.js
@@ -0,0 +1,24 @@
+{
+  preconception: {
+    "corticosteroidsbudesonide": "safe", "5-aminosalicylatesmesalamine": "safe",  "5-aminosalicylatessulfasalazine": "safe", "immunomodulatorimuran": "safe", "immunomodulatorpurinethol": "safe", "biologicsantiTnfs": "safe", "calcineurinInhibitorcyclosporin": "safe", "antibioticsciprofloxacin": "safe", "antibioticsmetronidazole": "safe", "antibioticspenicillin": "safe", "immunomodulatormethotrexate": "unsafe", "smallMoleculestofacitinib": "unsafe",
+    "biologicsustekinumab": "unknown", "biologicsvedolizumab": "unknown", "corticosteroidsprednisone": "caution", "corticosteroidsmethylprednisolone": "caution", "corticosteroidshydrocortisone": "caution", "immunomodulatorallopurinol": "caution"
+  },
+  pregnant: {
+    "corticosteroidsbudesonide": "safe", "5-aminosalicylatesmesalamine": "safe",  "5-aminosalicylatessulfasalazine": "safe", "immunomodulatorimuran": "safe", "immunomodulatorpurinethol": "safe", "biologicsantiTnfs": "safe", "antibioticspenicillin": "safe", "immunomodulatormethotrexate": "unsafe", "smallMoleculestofacitinib": "unsafe",
+    "biologicsustekinumab": "unknown", "biologicsvedolizumab": "unknown",
+    "corticosteroidsprednisone": "caution", "corticosteroidsmethylprednisolone": "caution", "corticosteroidshydrocortisone": "caution", "immunomodulatorallopurinol": "caution", "calcineurinInhibitorcyclosporin": "caution", "antibioticsciprofloxacin": "caution", "antibioticsmetronidazole": "caution"
+  },
+  breastfeeding: {
+    "corticosteroidsbudesonide": "safe", "5-aminosalicylatesmesalamine": "safe",  "5-aminosalicylatessulfasalazine": "safe", "immunomodulatorimuran": "safe", "immunomodulatorpurinethol": "safe", "biologicsantiTnfs": "safe", "antibioticsciprofloxacin": "safe", "immunomodulatormethotrexate": "unsafe", "smallMoleculestofacitinib": "unsafe",
+    "biologicsustekinumab":"unknown", "biologicsvedolizumab":"unknown",
+    "corticosteroidsprednisone": "caution", "corticosteroidsmethylprednisolone": "caution", "corticosteroidshydrocortisone": "caution", "immunomodulatorallopurinol": "caution", "calcineurinInhibitorcyclosporin": "caution", "antibioticsmetronidazole": "caution"
+  },
+  vaccinations: {
+    "corticosteroidsbudesonide": "safe", "5-aminosalicylatesmesalamine": "safe",  "5-aminosalicylatessulfasalazine": "safe", "immunomodulatorimuran": "safe", "immunomodulatorpurinethol": "safe", "calcineurinInhibitorcyclosporin": "safe",
+    "biologicsustekinumab": "unknown", "biologicsvedolizumab": "unknown", "smallMoleculestofacitinib": "unknown",
+    "corticosteroidsprednisone": "caution", "corticosteroidsmethylprednisolone": "caution", "corticosteroidshydrocortisone": "caution", "biologicsantiTnfs": "caution"
+  },
+  infections: {
+    "corticosteroidsbudesonide": "safe", "5-aminosalicylatesmesalamine": "safe",  "5-aminosalicylatessulfasalazine": "safe", "immunomodulatorimuran": "safe", "immunomodulatorpurinethol": "safe", "biologicsantiTnfs": "safe", "calcineurinInhibitorcyclosporin": "safe", "biologicsustekinumab": "unknown", "biologicsvedolizumab": "unknown", "smallMoleculestofacitinib": "unknown", "corticosteroidsprednisone": "caution", "methylprednisolonehydrocortisone": "caution",
+  }
+}
