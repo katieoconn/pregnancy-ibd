03d2a02b9a64b09c26be361310f32a95f509d62e
astridjanewilliams <54977888+astridjanewilliams@users.noreply.github.com>
2022-04-28 12:23:35 +1000

    Update formEntries.js

--- medications/formEntries.js
+++ medications/formEntries.js
@@ -1,8 +1,19 @@
 [{
-  "title": "aminosalicylates",
-  "options": ["no", "sulfasalazine", "otherAminosalicylates"]
+  "title": "NONE, I am not on any medications for IBD",
 },
-{
+ {
+  "title": "corticosteroids",
+  "options": ["no", "budesonide", "Prednisone/prednisolone", "Methylprednisolone","Hydrocortisone"]
+},  
+  {
+  "title": "5-Aminosalicylates",
+  "options": ["no", "Pentasa®, Asacol®, Salofalk®, Mesavant®, and generics"]
+},
+  {
+  "title": "sulfasalazine",
+  "options": ["no", "yes"]
+ },
+  {
   "title": "methotrexate",
   "options": ["no", "yes"]
 },
