ca08d86e4b4d9e05d4b2a09484fd9670d6367f96
katieoconn <54900717+katieoconn@users.noreply.github.com>
2022-08-15 15:11:32 -0400

    Update formEntries2.js

--- medications/formEntries2.js
+++ medications/formEntries2.js
@@ -6,8 +6,16 @@
 "title": "budesonide",
  "options": ["no", "yes"]
 },
- { 
+{ 
 "title": "prednisone",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "methylprednisolone",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "hydrocortisone",
  "options": ["no", "yes"]
 },
 {
@@ -15,22 +23,78 @@
   "options": ["no", "mesalamine", "sulfasalazine"]
 },
 { 
+"title": "mesalamine",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "sulfasalazine",
+ "options": ["no", "yes"]
+},
+{ 
  "title": "immunomodulator",
   "options": ["no", "imuran", "purinethol", "methotrexate", "allopurinol"]
+},
+{ 
+"title": "imuran",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "purinethol",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "methotrexate",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "allopurinol",
+ "options": ["no", "yes"]
 },
 {
   "title": "biologics",
   "options": ["no", "antiTnfs", "ustekinumab", "vedolizumab"]
 },
+{ 
+"title": "antiTnfs",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "ustekinumab",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "vedolizumab",
+ "options": ["no", "yes"]
+},
 {
   "title": "smallmolecules",
   "options": ["no", "tofacitinib"]
 },
-  {
+{ 
+"title": "tofacitinib",
+ "options": ["no", "yes"]
+},
+{
   "title": "calcineurinInhibitor",
   "options": ["no", "cyclosporin"]
+},
+{ 
+"title": "cyclosporin",
+ "options": ["no", "yes"]
 },
 {
   "title": "antibiotics",
   "options": ["no", "ciprofloxacin", "metronidazole", "penicillin"]
+}
+{ 
+"title": "ciprofloxacin",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "metronidazole",
+ "options": ["no", "yes"]
+},
+{ 
+"title": "penicillin",
+ "options": ["no", "yes"]
 }]
